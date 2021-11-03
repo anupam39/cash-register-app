cdfd26226985ac055631296ddbf1adfb9e5b18a7
anupam39 <anupamgupta31149@gmail.com>
2021-11-03 23:33:40 +0530

    basic logic implemented

--- app.js
+++ app.js
@@ -1,4 +1,36 @@
 const billAmount=document.querySelector("#bill-amount");
 const checkButton=document.querySelector("#check-button");
 const cashGiven=document.querySelector("#cash-given");
-console.log(cashGiven.value);
+const message=document.querySelector("#error-message");
+const availableNotes=[2000,500,200,100,50,20,10,5,2,1];
+const noOfNotes=document.querySelectorAll(".no-of-notes");
+
+checkButton.addEventListener("click", function validateBillAmount() {
+    hideMessage();
+    if(billAmount.value>0){
+        if(cashGiven.value >= billAmount.value){
+            const amountToBeReturned = cashGiven.value - billAmount.value;
+            calculateChange(amountToBeReturned);
+        } else{
+            showMessage("The cash provided should be more than or equal to the bill.");
+              }
+    }     else{
+            showMessage("Invalid Bill Amount");
+              }
+});
+function calculateChange(amountToBeReturned){
+    for(let i=0; i<availableNotes.length; i++){
+        const numberOfNotes = Math.trunc(amountToBeReturned/availableNotes[i]);
+        amountToBeReturned = amountToBeReturned % availableNotes[i];
+        noOfNotes[i].innerText = numberOfNotes;
+    }
+}
+
+function hideMessage(){
+    message.style.display="none";
+}
+
+function showMessage(msg){
+    message.style.display="block";
+    message.innerText = msg;
+}
