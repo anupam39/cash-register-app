71e3c5d5320d261e341aae6e8627b1f6b33f31d1
anupam39 <anupamgupta31149@gmail.com>
2021-11-03 16:58:19 +0530

    all the components have been added

---
+++ app.js
@@ -0,0 +1,4 @@
+const billAmount=document.querySelector("#bill-amount");
+const checkButton=document.querySelector("#check-button");
+const cashGiven=document.querySelector("#cash-given");
+console.log(cashGiven.value);
